d83f31e9211ab3e1472161a199874cbcd79b30e1
Linex77 <cosadiz6969@gmail.com>
2018-01-18 14:28:19 +0800

    first commit

--- example/tcr.py
+++ example/tcr.py
@@ -196,9 +196,9 @@ def bot(op):
                         Ti = cl.reissueGroupTicket(op.param1)
 
         if op.type == 13:
-            print op.param1
-            print op.param2
-            print op.param3
+            print op.param1#GROUP
+            print op.param2#PELAKU
+            print op.param3#KORBAN
             if mid in op.param3:
                 G = cl.getGroup(op.param1)
                 if wait["autoJoin"] == True:
